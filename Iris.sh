21d5dd3e5c02fb020d46618471a0aab40a8cce4a
Jacilop980 <jacilop980@gmail.com>
2026-09-11 11:42:40 -0600

    Prueba de github

--- Iris.sh
+++ Iris.sh
@@ -5,5 +5,6 @@ sudo systemctl restart systemd-timesyncd && timedatectl status
 bash ~/Desktop/InspectionApp/setup/start_iris.sh
 
 # Esto es para iniciar manualmente
+# Y esto para iniciar automáticamente al encender la computadora
 echo "Proceso finalizado. Presiona Enter para cerrar esta ventana."
 read
