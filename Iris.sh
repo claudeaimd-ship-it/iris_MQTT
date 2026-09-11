de9b8174fa46761bea1d45675066f2699348c1f4
AlanJafetRL <alanjafet.rl@gmail.com>
2026-09-11 11:39:58 -0600

    Prueba comentario Alan

--- Iris.sh
+++ Iris.sh
@@ -5,5 +5,6 @@ sudo systemctl restart systemd-timesyncd && timedatectl status
 bash ~/Desktop/InspectionApp/setup/start_iris.sh
 
 # Esto es para iniciar manualmente
+# Mas comentarios de prueba
 echo "Proceso finalizado. Presiona Enter para cerrar esta ventana."
 read
