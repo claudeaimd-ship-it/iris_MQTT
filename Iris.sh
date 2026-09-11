453466ac9be95622e9fbffcf32764730365721f8
javierjetc9gen-sudo <javierjetc9@gmail.com>
2026-09-11 11:56:29 -0600

    Prueba comentario mia

--- Iris.sh
+++ Iris.sh
@@ -7,5 +7,6 @@ bash ~/Desktop/InspectionApp/setup/start_iris.sh
 # Esto es para iniciar manualmente
 # Y esto para iniciar automáticamente al encender la computadora
 # Mas comentarios de prueba
+#Comentario mio
 echo "Proceso finalizado. Presiona Enter para cerrar esta ventana."
 read
